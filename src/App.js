3dacb8851342ffdcbafc10f4ace056e6d3cf0f35
lostcode <lostcoding11@gmail.com>
2022-05-06 15:18:26 +0300

    notes app completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Note from "./components/note";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Col, Container, Row } from "react-bootstrap";
+import NoteForm from "./components/note-form";
+import Notes from "./components/notes";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    loadNotes();
+  }, []);
+
+  const loadNotes = () => {
+    axios("https://jsonplaceholder.typicode.com/posts").then((resp) =>
+      setNotes(resp.data)
+    );
+  };
+
+  const removeNote = (id) => {
+    if (!id) return;
+
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((resp) => {
+        const arr = notes.filter((item) => item.id != id);
+        setNotes(arr);
+        alert("Notes was deleted");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("An error occured");
+      });
+  };
+
+  const createNote = (note) => {
+    alert("Notes added");
+    axios
+      .post("https://jsonplaceholder.typicode.com/posts", note)
+      .then((resp) => {
+        setNotes([resp.data, ...notes]);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row className="g-5">
+        <Col md={3}>
+          <NoteForm createNote={createNote} />
+        </Col>
+        <Col md={9}>
+          <Notes data={notes} removeNote={removeNote} />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
